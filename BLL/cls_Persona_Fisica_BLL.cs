203e93c6bd369ab79e3eeae8f09b2b448134f2b8
Oliver J. Pereira Villa <oliverpereirajv@gmail.com>
2022-12-02 21:07:42 -0600

    Modifique los comentarios

--- BLL/cls_Persona_Fisica_BLL.cs
+++ BLL/cls_Persona_Fisica_BLL.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace BLL
 {   /* HOLA COMPAÑEROS SALUDITOS */
+ 
     /* HOLA COMPAÑEROS SALUDITOS */
-    /* HOLA COMPAÑEROS SALUDITOS */
+    /* HOLA COMPAÑEROS SALUDITOS */ 
+    /* HOLA COMPAÑEROS SALUDITOS */ 
+    /* HOLA COMPAÑEROS SALUDITOS */ 
+    /* HOLA COMPAÑEROS SALUDITOS */ 
+    /* HOLA COMPAÑEROS SALUDITOS */ 
+    /* HOLA COMPAÑEROS SALUDITOS */ 
     /* HOLA COMPAÑEROS SALUDITOS */ 
   
     public class cls_Persona_Fisica_BLL
